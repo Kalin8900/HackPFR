3c576850e22f3f9163ef12764057906b7e734d04
Mikołaj Domagalski <mikolaj.domagalski@op.pl>
2020-11-21 21:57:05 +0100

    .

--- hackpfr/src/screens/DeclarationsScreen.js
+++ hackpfr/src/screens/DeclarationsScreen.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import declarations from '../assets/geodata/declarations_done.json';
+import markerIcon from "../assets/graphics/marker.png"
 import L from "leaflet";
 
 let mymap;
@@ -11,6 +13,19 @@ const CollectionsMapScreen = () => {
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(mymap)
+
+        let markerImage = L.icon({
+            iconUrl: markerIcon,
+            iconSize: [25, 25],
+            iconAnchor: [20, 20],
+            popupAnchor: [-30, -76]
+        });
+
+        for (let i = 0; i < declarations.features.length; i++) {
+            ;
+        }
+
+        //mymap.addLayer(markers);
     }, []);
 
     return (
