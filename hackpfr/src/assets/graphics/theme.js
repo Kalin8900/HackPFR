9311d68d3a307fd569ac3b79470f4d7cc9acdee6
Kalin8900 <Kalin8900@gmail.com>
2020-11-21 13:26:37 +0100

    Styling Home screen

---
+++ hackpfr/src/assets/graphics/theme.js
@@ -0,0 +1,18 @@
+const mainTheme = {
+    colors: {
+        pageBackground: 'rgba(232, 239, 239, 1)',
+        accent: 'rgba(186, 235, 135, 1)',
+        white: 'rgb(255, 255, 255)',
+        black:'rgb(51, 51, 51)',
+        buttonShadow: 'rgba(191, 222, 222, 1)',
+    },
+    font: {
+        size: {
+            buttonSize: 40
+        },
+        defaultFont: 'Manrope, sans-serif',
+    }
+
+}
+
+export default mainTheme;
--- hackpfr/src/screens/HomeScreen.js
+++ hackpfr/src/screens/HomeScreen.js
@@ -2,22 +2,43 @@ import React, {useState, Component} from "react";
 import {Navbar, Nav, NavDropdown} from 'react-bootstrap';
 import {Container, Row, Col, Button} from 'react-bootstrap';
 import BigButton from "../components/BigButton";
+import mainTheme from "../assets/graphics/theme";
+const mt = mainTheme;
+
+const btnStyle = {
+    background: mt.colors.white,
+    borderRadius: '2%',
+    border: 0,
+    color: mt.colors.black,
+    fontWeight: 600,
+    fontSize: mt.font.size.buttonSize,
+    boxShadow: '10px 10px ' + mt.colors.buttonShadow
+}
+
+const coloredBtnStyle = {
+    ...btnStyle,
+    marginBottom: '1vw'
+};
+coloredBtnStyle.background = mt.colors.accent;
+coloredBtnStyle.color = mt.colors.white;
 
 function HomeScreen() {
     return (
-        <Container>
-            <Row>
-                <Col>
-                    <BigButton className="homeBtn" path='/throw'>Oddaj śmieci</BigButton>
-                </Col>
-                <Col>
-                    <BigButton className='smallHomeBtn' path='/reportCollection'>Zgłoś zbiórkę</BigButton>
-                    <BigButton className='smallHomeBtn' path='/update'>Aktualizuj zbiórkę</BigButton>
-                </Col>
-                <Col>
-                    <BigButton className='homeBtn' path='/reportIsolation'>Zgłoś izolację</BigButton>
-                </Col>
-            </Row>
+        <Container className='page' style={{background: mt.colors.pageBackground}}>
+            <Container>
+                <Row>
+                    <Col>
+                        <BigButton className="homeBtn" style={btnStyle} path='/throw'>Akutalne zbiórki</BigButton>
+                    </Col>
+                    <Col>
+                        <BigButton className='smallHomeBtn' style={coloredBtnStyle} path='/reportCollection'>Zgłoś zbiórkę</BigButton>
+                        <BigButton className='smallHomeBtn' style={btnStyle} path='/update'>Zaktualizuj zbiórkę</BigButton>
+                    </Col>
+                    <Col>
+                        <BigButton className='homeBtn' style={btnStyle} path='/reportIsolation'>Zgłoś izolację</BigButton>
+                    </Col>
+                </Row>
+            </Container>
         </Container>
     );
 }
