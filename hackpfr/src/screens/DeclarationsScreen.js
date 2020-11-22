128535669390dd0962d330885ca2c8244f2b834a
Mikołaj Domagalski <mikolaj.domagalski@op.pl>
2020-11-22 11:13:38 +0100

    clusters!!! xD

--- hackpfr/src/screens/DeclarationsScreen.js
+++ hackpfr/src/screens/DeclarationsScreen.js
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from "react";
 import declarations from '../assets/geodata/declarations_done.json';
-import markerIcon from "../assets/graphics/marker.png"
+import markerIconGreen from "../assets/graphics/pin-doc-zielony.svg";
+import markerIconBlue from "../assets/graphics/pin-doc-niebieski.svg";
+import markerIconBlack from "../assets/graphics/pin-doc-czarny.svg";
 //import { MarkerCluster } from '../components/clusters/src/MarkerCluster.js';
 //import { MarkerClusterGroup } from '../components/clusters/src/MarkerClusterGroup.js';
+import * as clusters from '../components/cluster/leaflet.markercluster/dist/leaflet.markercluster.js';
+import '../components/cluster/leaflet.markercluster/dist/MarkerCluster.css';
+import '../components/cluster/leaflet.markercluster/dist/MarkerCluster.Default.css';
 import L from "leaflet";
 import PermissionDenied from "../components/PermissionDenied";
 
@@ -12,7 +17,7 @@ const CollectionsMapScreen = (props) => {
     const user = props.user;
 
     useEffect(() => {
-        if(user.displayName !== 'urząd')
+        if (user.displayName !== 'urząd')
             return;
         //map init
         mymap = L.map('mapid').setView([51.132541, 23.479241], 14, { zoomControl: true });
@@ -22,40 +27,41 @@ const CollectionsMapScreen = (props) => {
         }).addTo(mymap)
 
         let markerGreen = L.icon({
-            iconUrl: markerIcon,
+            iconUrl: markerIconGreen,
             iconSize: [25, 25],
             iconAnchor: [20, 20],
             popupAnchor: [-30, -76]
         });
 
         let markerBlue = L.icon({
-            iconUrl: markerIcon,
+            iconUrl: markerIconBlue,
             iconSize: [25, 25],
             iconAnchor: [20, 20],
             popupAnchor: [-30, -76]
         });
 
         let markerBlack = L.icon({
-            iconUrl: markerIcon,
+            iconUrl: markerIconBlack,
             iconSize: [25, 25],
             iconAnchor: [20, 20],
             popupAnchor: [-30, -76]
         });
-/*
-		let markers = L.markerClusterGroup();
+
+        let markers = L.markerClusterGroup({maxClusterRadius: 40});
 
         for (let i = 0; i < declarations.features.length; i++) {
             let a = declarations.features[i].geometry.coordinates;
-            let type = eclarations.features[i].properties.type;
-			let marker = L.marker(new L.LatLng(a[0], a[1]), { title: "title", icon: (type === 'Mieszkalna' ? markerGreen : (type === 'Niemieszkalna' ? markerBlue : markerBlack)) });
-			marker.bindPopup("title");
-			markers.addLayer(marker);
-		}
-		mymap.addLayer(markers);
-*/
+            let type = declarations.features[i].properties.type;
+            let marker = L.marker(new L.LatLng(a[1], a[0]), { title: "title", icon: (type === 'Mieszkalna' ? markerGreen : (type === 'Niemieszkalna' ? markerBlue : markerBlack)) });
+            marker.bindPopup("<b>" + declarations.features[i].properties.ULica_nr + "</b>");
+            markers.addLayer(marker);
+        }
+        
+        mymap.addLayer(markers);
+
     }, []);
 
-    if(user.displayName !== 'urząd')
+    if (user.displayName !== 'urząd')
         return <PermissionDenied />
 
     return (
