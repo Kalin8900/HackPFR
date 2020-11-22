739990850aea8fa96499d347755b63120cb2c034
Kalin8900 <Kalin8900@gmail.com>
2020-11-22 04:35:28 +0100

    fake chart

--- hackpfr/src/screens/StatsScreen.js
+++ hackpfr/src/screens/StatsScreen.js
@@ -11,10 +11,42 @@ const mt = mainTheme;
 const RowStyle = {display: 'flex', alignItems: 'flex-end', justifyContent: 'center', width: '50vw',gap: 0}
 const StatsScreen = props => {
 
+    const traces = {
+        trace1: {
+            x: ['sty', 'lut', 'mar', 'kwi', 'maj', 'cze', 'lip'],
+            y: [50, 55, 65, 54, 78, 87, 64],
+            name: 'Butelki PET',
+            mode: 'lines',
+            line: {
+                color: 'rgb(235,252,3)',
+                width: 2
+            }
+        },
+        trace2: {
+            x: ['sty', 'lut', 'mar', 'kwi', 'maj', 'cze', 'lip'],
+            y: [43, 32, 24, 49, 61, 52, 33],
+            name: 'Metal',
+            mode: 'lines',
+            line: {
+                color: 'rgb(53,55,56)',
+                width: 2
+            }
+        },
+        trace3: {
+            x: ['sty', 'lut', 'mar', 'kwi', 'maj', 'cze', 'lip'],
+            y: [89, 54, 84, 90, 56, 54, 76],
+            name: 'Makulatura',
+            mode: 'lines',
+            line: {
+                color: 'rgb(3, 186, 252)',
+                width: 2
+            }
+        },
+    }
 
     return (
         <Container className='page' style={{backgroundColor: mt.colors.pageBackground, justifyContent: 'center', flexFlow: 'column'}}>
-            <Row style={RowStyle}>Najlepsi w tym miesiącu</Row>
+            <Row style={{...RowStyle, marginTop: '4vh'}}><h2>Najlepsi w tym miesiącu</h2></Row>
             <Row style={{display: 'flex', alignItems: 'flex-end', justifyContent: 'center', width: '50vw',gap: 0}}>
                 <Col>
                     <h3>szkoła 1.</h3>
@@ -28,22 +60,11 @@ const StatsScreen = props => {
                     <h3> szkoła 3.</h3>
                     <div style={{background: 'red', height:100}}></div>
                 </Col>
-            </Row><Row style={{display: 'flex', alignItems: 'flex-end', justifyContent: 'center', width: '50vw',gap: 0}}>
-            {/*<Plot*/}
-        </Row><Row style={{display: 'flex', alignItems: 'flex-end', justifyContent: 'center', width: '50vw',gap: 0}}>
-            <Col>
-                <h3>szkoła 1.</h3>
-                <div style={{background: 'red', height:175}}></div>
-            </Col>
-            <Col>
-                <h3>szkoła 2.</h3>
-                <div style={{background: 'red', height:250}}></div>
-            </Col>
-            <Col>
-                <h3> szkoła 3.</h3>
-                <div style={{background: 'red', height:100}}></div>
-            </Col>
-        </Row>
+            </Row>
+            <Row style={{display: 'flex', alignItems: 'flex-end', justifyContent: 'center', width: '50vw',gap: 0, marginTop: '6vh'}}>
+            <Plot data={[traces.trace1, traces.trace2, traces.trace3]} layout={{title: 'Ilość zebranych surowców w danym miesiącu'}} />
+            </Row>
+        
         </Container>
     )
 }
