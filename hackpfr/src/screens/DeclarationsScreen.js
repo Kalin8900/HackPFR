374fe68e235c1247dbf277ea05baf7654ab0009b
Mikołaj Domagalski <mikolaj.domagalski@op.pl>
2020-11-22 10:44:15 +0100

    markers colors

--- hackpfr/src/screens/DeclarationsScreen.js
+++ hackpfr/src/screens/DeclarationsScreen.js
@@ -21,7 +21,21 @@ const CollectionsMapScreen = (props) => {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(mymap)
 
-        let markerImage = L.icon({
+        let markerGreen = L.icon({
+            iconUrl: markerIcon,
+            iconSize: [25, 25],
+            iconAnchor: [20, 20],
+            popupAnchor: [-30, -76]
+        });
+
+        let markerBlue = L.icon({
+            iconUrl: markerIcon,
+            iconSize: [25, 25],
+            iconAnchor: [20, 20],
+            popupAnchor: [-30, -76]
+        });
+
+        let markerBlack = L.icon({
             iconUrl: markerIcon,
             iconSize: [25, 25],
             iconAnchor: [20, 20],
@@ -31,8 +45,9 @@ const CollectionsMapScreen = (props) => {
 		let markers = L.markerClusterGroup();
 
         for (let i = 0; i < declarations.features.length; i++) {
-			let a = declarations.features[i].geometry.coordinates;
-			let marker = L.marker(new L.LatLng(a[0], a[1]), { title: "title" });
+            let a = declarations.features[i].geometry.coordinates;
+            let type = eclarations.features[i].properties.type;
+			let marker = L.marker(new L.LatLng(a[0], a[1]), { title: "title", icon: (type === 'Mieszkalna' ? markerGreen : (type === 'Niemieszkalna' ? markerBlue : markerBlack)) });
 			marker.bindPopup("title");
 			markers.addLayer(marker);
 		}
